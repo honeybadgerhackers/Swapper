c3d443b4e67bd36d5d26c2805dabf1d365d35e0c
Hunter Ledet <hunter.ledet@gmail.com>
2018-01-04 13:39:17 -0600

    [cloud-hosting] enable cloud hosting of locally stored images

--- src/components/addItem.vue
+++ src/components/addItem.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   <template>
   <div class="addItem">
       <b-button @click="showModal" class="btn btn-lg btn-test float-left">Add New Item</b-button>
@@ -16,7 +15,7 @@
                   </b-dropdown>
                 </div>
                 <br>
-                <form>
+                <form enctype="multipart/form-data">
                   <div class="form-group">
                     <label for="titleArea">Item Name</label>
                     <input v-model="name" type="name" class="form-control" id="titleArea" placeholder="Enter Item Name">
@@ -28,6 +27,10 @@
                   <div class="form-group">
                     <label for="descriptionArea">Image URL (optional)</label>
                     <input v-model="imageUrl" type="text" class="form-control" id="urlArea" placeholder="image URL (optional)"/>
+                  </div>
+                  <div class="form-group">
+                    <label for="descriptionArea">Upload images (optional)</label>
+                    <input type="file" class="form-control" v-on:change="upload($event.target.files)" accept="image/*" />
                   </div>
                 </form>
             </div>
@@ -41,6 +44,7 @@
 
 <script>
 import axios from 'axios';
+import CLOUDINARY_VAR from '../assets/cloudinary.config';
 
 export default {
   name: 'addItem',
@@ -54,9 +58,37 @@ export default {
       selectedCategory: 'Categories',
       categoryId: null,
       imageUrl: '',
+      cloudinary: {
+        uploadPreset: 'x8p5gkk3',
+        cloudName: CLOUDINARY_VAR.cloud_name,
+      },
+      thumb: {
+        url: '',
+      },
+      thumbs: [],
     };
   },
   methods: {
+    upload(file) {
+      console.log(this.cloudinary)
+      const formData = new FormData();
+      formData.append('file', file[0]);
+      formData.append('upload_preset', this.cloudinary.uploadPreset);
+      formData.append('tags', 'gs-vue, gs-vue-uploaded');
+      // for debugging to inspect content on formData
+      for (const pair of formData.entries()) {
+        console.log(`${pair[0]}, ${pair[1]}`, 'this is pair from formData.entries');
+      }
+      axios.post(`https://api.cloudinary.com/v1_1/legacy-swappr/image/upload`, formData)
+        .then((res) => {
+          console.log(res);
+          this.imageUrl = res.data.secure_url;
+          this.thumbs.unshift({
+            url: res.data.secure_url,
+          });
+        })
+        .catch(err => console.error(err.response));
+    },
     showModal() {
       this.$refs.addItemModal.show();
     },
@@ -65,6 +97,7 @@ export default {
     },
     addItem() {
       let newImage;
+      console.log(this.thumbs);
       // this is where i could take in this.imageUrl, and ensure that url_img is a cloudinary url
       // axios.post to route setup to handle image upload, come back with image url,
       // in my .then have the rest of this code.
@@ -177,159 +210,3 @@ li {
     overflow-x: hidden;
 }
 </style>
-=======
-  <template>
-  <div class="addItem">
-      <b-button @click="showModal" class="btn btn-lg btn-test float-left">Add New Item</b-button>
-      <b-modal ref="addItemModal" class="mt-10">
-            <div slot="modal-header" class="w-100">
-              <h3 class="float-left">Add New Item</h3>
-              <button class="close float-right" @click="hideModal">&times;</button>
-            </div>
-            <div class="modal-body">
-                <div>
-                  <b-dropdown id="ddown1" :text="selectedCategory" class="m-md-2">
-                    <div class="scrollable-menu">
-                      <b-dropdown-item v-for="(category,index) in categories" :category='category' :key='index' @click="dropdownClick(category)">{{ category.name }}</b-dropdown-item>
-                    </div>
-                  </b-dropdown>
-                </div>
-                <br>
-                <form>
-                  <div class="form-group">
-                    <label for="titleArea">Item Name</label>
-                    <input v-model="name" type="name" class="form-control" id="titleArea" placeholder="Enter Item Name">
-                  </div>
-                  <div class="form-group">
-                    <label for="descriptionArea">Description</label>
-                    <textarea v-model="description" v-on:keyup.enter="addItem" type="text" class="form-control" id="descriptionArea" placeholder="description"></textarea>
-                  </div>
-                  <div class="form-group">
-                    <label for="descriptionArea">Image URL (optional)</label>
-                    <input v-model="imageUrl" type="text" class="form-control" id="urlArea" placeholder="image URL (optional)"></input>
-                  </div>
-                </form>
-            </div>
-            <div slot="modal-footer" class="w-100">
-              <b-btn class="float-left" variant="primary" @click="hideModal">Close</b-btn>
-              <button @click="addItem" type='button' class="btn btn-primary float-right">Add Item</button>
-            </div>
-      </b-modal>          
-   </div>
-</template>
-
-<script>
-import axios from 'axios';
-
-export default {
-  name: 'addItem',
-  props: ['auth', 'authentication', 'userId'],
-  data() {
-    return {
-      newItem: null,
-      name: '',
-      description: '',
-      categories: [],
-      selectedCategory: 'Categories',
-      categoryId: null,
-      imageUrl: '',
-    };
-  },
-  methods: {
-    showModal() {
-      this.$refs.addItemModal.show();
-    },
-    hideModal() {
-      this.$refs.addItemModal.hide();
-    },
-    addItem() {
-      if (this.name.length !== 0 && this.description.length !== 0 && this.categoryId !== null) {
-        const config = {
-          name: this.name,
-          description: this.description,
-          id_user: this.userId,
-          id_category: this.categoryId,
-          url_img: this.imageUrl.length ? this.imageUrl : null,
-        };
-        this.hideModal();
-        axios.post('/items', config)
-          .then((item) => {
-            this.name = '';
-            this.description = '';
-            this.imageUrl = '';
-            this.$emit('new-item', item);
-            this.categoryId = null;
-            this.selectedCategory = 'Categories';
-          })
-          .catch(err => console.error(err));
-      }
-    },
-    getCategories() {
-      axios.get('/categories')
-      .then(({ data: categories }) => {
-        this.categories = categories;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    },
-    dropdownClick({ id, name }) {
-      this.selectedCategory = name;
-      this.categoryId = id;
-    },
-  },
-  mounted() {
-    this.getCategories();
-  },
-};
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-.modal-content {
-  margin-top: 25vh;
-}
-
-.modal-header {
-  text-align: left;
-}
-
-.modal-body {
-  font-size: 14px;
-  text-align: left;
-}
-
-textarea {
-  resize: none;
-}
-
-li {
-  display: inline-block;
-}
-
-.btn-add-item {
-  background-color: #029ba1;
-  color: #f0ffff;
-}
-
-.btn-add-item:hover {
-  background-color: #028489;
-  color: #f0ffff;
-}
-
-.btn-add-item:active {
-  background-color: #359486;
-}
-
-.scrollable-menu {
-    height: auto;    
-    max-height: 300px;
-    overflow-x: hidden;
-}
-</style>
->>>>>>> ea7a79ed159fdf9b19a7a831385d838063f372db
