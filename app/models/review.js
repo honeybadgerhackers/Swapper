1231e75cf362aaf22ae77c235d71ad27b85c9427
Eddie Smith, Jr <eddielsjr@gmail.com>
2018-01-04 11:30:25 -0600

    [add] review table to db

--- app/models/review.js
+++ app/models/review.js
@@ -1,13 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
     comment: DataTypes.STRING,  // the comment submitted as a review
-    id_user: DataTypes.BIGINT,  // the id of the user being reviewed,
     rating: DataTypes.BIGINT,   // a rating scale of 1 - 5, 5 being the best
+    id_reviewer: DataTypes.BIGINT,  // the id of the user giving the review
+    id_reviewee: DataTypes.BIGINT,  // the id of the user being reviewed
   });
 
   Review.associate = (models) => {
     Review.belongsTo(models.User, {
-      foreignKey: 'id_user',
+      foreignKey: 'id_reviewee',
+    });
+
+    Review.belongsTo(models.User, {
+      foreignKey: 'id_reviewer',
     });
   };
   return Review;
